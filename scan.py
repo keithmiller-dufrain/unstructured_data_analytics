42b15d00f445e689bf886a1d453cbe0578272a11
fraser1234 <fraseralexanderblack@gmail.com>
2019-12-06 22:42:06 +0000

    next step: read in file and print out metadata

--- scan.py
+++ scan.py
@@ -6,52 +6,52 @@ from pwd import getpwuid
 
 
 # get filename and store it in a variable, do this in other script and read in results?
-def get_filename():
-
-
+def get_file_path():
+    file_path = ‎⁨'~/Macintosh HD⁩/Users⁩/fraserblack⁩/Desktop⁩/Dufrain⁩/Insight into Unstructured Data.docx'
+    return file_path
 
 # get file creation date
-def get_file_creation_date(filename):
-    c = os.path.getctime(filename)
+def get_file_creation_date(file_path):
+    c = os.path.getctime(file_path)
     print(dt.datetime.fromtimestamp(c))
     return dt.datetime.fromtimestamp(c)
 
 
 # get file modification date
-def get_modification_date(filename):
-    m = os.path.getmtime(filename)
+def get_modification_date(file_path):
+    m = os.path.getmtime(file_path)
     print(dt.datetime.fromtimestamp(m))
     return dt.datetime.fromtimestamp(m)
 
 
 # get last accessed
-def get_last_accessed_date(filename):
-    a = os.path.getatime(filename)
+def get_last_accessed_date(file_path):
+    a = os.path.getatime(file_path)
     print(dt.datetime.fromtimestamp(a))
     return dt.datetime.fromtimestamp(a)
 
 
 # get file size, will need to edit with sizes and make uniform e.g all MB or GB.
-def get_file_size(filename):
-    s = os.path.getsize(filename)
+def get_file_size(file_path):
+    s = os.path.getsize(file_path)
     print(s)
     return s
 
 
 # get file owner/creator
-def get_file_owner(filename):
-    o = getpwuid(stat(filename).st_uid).pw_name
+def get_file_owner(file_path):
+    o = getpwuid(stat(file_path).st_uid).pw_name
     print(o)
     return o
 
 
 if __name__ == '__main__':
-    get_filename()
-    get_file_creation_date()
-    get_modification_date()
-    get_last_accessed_date()
-    get_file_owner()
-    get_file_size()
+    get_file_path()
+    get_file_creation_date(file_path)
+    get_modification_date(file_path)
+    get_last_accessed_date(file_path)
+    get_file_owner(file_path)
+    get_file_size(file_path)
 
 
 
