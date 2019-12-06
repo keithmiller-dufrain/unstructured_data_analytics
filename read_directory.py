95fbf076c401c25d6216f88edf62b3a136b5748f
fraser1234 <fraseralexanderblack@gmail.com>
2019-12-06 20:49:18 +0000

    four basic functions

---
+++ read_directory.py
@@ -0,0 +1,1 @@
+# script to inspect a given directory
--- scan.py
+++ scan.py
@@ -1,6 +1,8 @@
 # import libraries
 import os
+from os import stat
 import datetime as dt
+from pwd import getpwuid
 
 # get filename
 
@@ -17,9 +19,19 @@ def modification_date(filename):
     return dt.datetime.fromtimestamp(m)
 
 
-# get file size
+# get file size, will need to edit with sizes and make uniform e.g all MB or GB.
 def get_file_size(filename):
-
+    s = os.path.getsize(filename)
+    return s
 
 #get file owner/creator
 def get_file_owner(filename):
+    o = getpwuid(stat(filename).st_uid).pw_name
+    return o
+
+if __name__ == '__main__':
+    get_file_creation_date()
+    get_file_owner()
+    get_file_size()
+
+
