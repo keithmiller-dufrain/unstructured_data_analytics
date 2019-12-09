4176ef873ad6197111f4d3f07262ecd2871a9b48
fraser1234 <fraseralexanderblack@gmail.com>
2019-12-09 20:48:21 +0000

    added in file info function

--- scan.py
+++ scan.py
@@ -9,25 +9,6 @@ import gather_data
 import time
 
 
-# empty list for file locations to scan
-file_paths = []
-
-# get filename and store it in a variable, do this in other script and read in results?
-def get_file_path(directory):
-    directory_len = read_directory.list_directory(directory)
-    length = len(directory_len)
-    for file in length(directory):
-        # for each file in the directory, return the full path for passing into information gathering functions. store in character string 
-        file_path = os.path.abspath(file)
-        file_paths.insert(file_path)
-
-
-        #print(file_path)
-        #return file_path
-        #file_path = ‎⁨'~/Macintosh HD⁩/Users⁩/fraserblack⁩/Desktop⁩/Dufrain⁩/Insight into Unstructured Data.docx'
-    
-
-
 # get file creation date
 def get_file_creation_date(file_path):
     c = time.strftime('%d/%m/%Y', time.gmtime(os.path.getmtime(file_path)))
@@ -64,14 +45,31 @@ def get_file_owner(file_path):
     return o
 
 
+# get filename and store it in a variable, do this in other script and read in results?
+def get_file_path(directory):
+    file_paths = []
+    directory_len = read_directory.list_directory(directory)
+    length = len(directory_len)
+    for file in length(directory):
+        # for each file in the directory, return the full path for passing into information gathering functions. store in character string 
+        file_path = os.path.abspath(file)
+        file_paths.insert(file_path)
+    
+
+def gather_file_info(file_paths):
+    for file in file_paths:
+        get_file_owner(file)
+        get_file_size(file)
+        get_file_creation_date(file)
+        get_modification_date(file)
+        get_last_accessed_date(file)
+
+
+
 if __name__ == '__main__':
-    read_directory.list_directory(Path.cwd())
     get_file_path(directory)
-    get_file_creation_date(file_path)
-    get_modification_date(file_path)
-    get_last_accessed_date(file_path)
-    get_file_owner(file_path)
-    get_file_size(file_path)
+    gather_file_info(directory)
+    
 
 
 
