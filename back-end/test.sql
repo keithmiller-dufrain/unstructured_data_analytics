5854885aaa6c2a3c4112b50407b8f06ae8c666ea
fraser1234 <fraseralexanderblack@gmail.com>
2019-12-14 10:29:18 +0100

    removing sql

--- back-end/test.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO test_table  (filename, owner, size, creation_date, modification_date, accessed_date, extension)  VALUES  ('read_directory.py', 'fraserblack', '384', '10_12_2019', '12_12_2019', '12_12_2019', 'py') ;
-INSERT INTO test_table  (filename, owner, size, creation_date, modification_date, accessed_date, extension)  VALUES  ('scan.py', 'fraserblack', '3966', '12_12_2019', '12_12_2019', '12_12_2019', 'py') ;
-INSERT INTO test_table  (filename, owner, size, creation_date, modification_date, accessed_date, extension)  VALUES  ('test_scan.py', 'fraserblack', '1692', '10_12_2019', '12_12_2019', '12_12_2019', 'py') ;
