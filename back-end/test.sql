aa7254e745e46ece86982277eaccd096365ea48b
fraser1234 <fraseralexanderblack@gmail.com>
2019-12-14 11:20:40 +0100

    adding sql

---
+++ back-end/test.sql
@@ -0,0 +1,3 @@
+INSERT INTO test_table  (filename, owner, size, creation_date, modification_date, accessed_date, extension)  VALUES  ('read_directory.py', 'fraserblack', '384', '10_12_2019', '12_12_2019', '14_12_2019', 'py') ;
+INSERT INTO test_table  (filename, owner, size, creation_date, modification_date, accessed_date, extension)  VALUES  ('scan.py', 'fraserblack', '3965', '12_12_2019', '12_12_2019', '14_12_2019', 'py') ;
+INSERT INTO test_table  (filename, owner, size, creation_date, modification_date, accessed_date, extension)  VALUES  ('test_scan.py', 'fraserblack', '1692', '10_12_2019', '12_12_2019', '14_12_2019', 'py') ;
